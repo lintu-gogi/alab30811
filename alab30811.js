83b7973dde838bd5bcca0dc358ae445eeef72c8a
lintu.gogi <lintu.joseph06@gmail.com>
2024-05-21 13:53:55 -0400

    second changes

--- alab30811.js
+++ alab30811.js
@@ -1,9 +1,11 @@
 // The initial numbers that must be verified.
-const n1 = 10;
-const n2 = 15;
+const n1 = 15;
+const n2 = 10;
 const n3 = 20;
 const n4 = 5;
-
+let isDivisibleBy5;
+let compareNum1AndNum2;
+let reminder;
 // Check one: add up to 50
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
@@ -18,8 +20,8 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+//const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
@@ -30,16 +32,36 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 // Finally, log the results.
-console.log(isValid);
+if(isValid){
+console.log(`The four numbers meet all the criteria and therefore : ${isValid}`);
+}
+else{
+    console.log(`The four numbers doesn't meet all the criteria and therefore : ${isValid}`);
+}
+isDivisibleBy5= (n1 % 5)+(n2 % 5)+(n3 % 5)+(n4 % 5)
 
+if(isDivisibleBy5>0)
+{
+    console.log('The all four numbers is not divisible by 5');
+}
+else{
+    console.log('The all four numbers is divisible by 5');
+}
+compareNum1AndNum2 = n1>n2;
+if(compareNum1AndNum2)
+console.log("First number is larger than the Second number ");
+else
+console.log("First number is smaller than the Second number ");
+reminder=((n2-n1)*n3)%n4;
+console.log(`The reminder after the arithmetic operations : ${reminder}`);
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+/*const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);*/
